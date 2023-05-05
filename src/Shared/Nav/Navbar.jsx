68adbe4b888d071963a1f543ac5f40b43392f062
Istaik Ahamad <ahmedruhanbmarpc@gmail.com>
2023-05-05 20:07:57 +0600

    another food section added

--- src/Shared/Nav/Navbar.jsx
+++ src/Shared/Nav/Navbar.jsx
@@ -1,20 +1,20 @@
-import React, { useContext } from 'react';
-import { BiUserCircle } from 'react-icons/bi';
-import { Link } from 'react-router-dom';
-import { AuthContext } from '../../Provider/AuthProvider';
+import React, { useContext } from "react";
+import { BiUserCircle } from "react-icons/bi";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../../Provider/AuthProvider";
 
 const Navbar = () => {
-  const {user, logOut} = useContext(AuthContext)
-    const handleLogOut =() =>{
-        logOut()
-        .then()
-        .catch(error =>{
-            console.log(error.message)
-        })
-    }
-    return (
-        <div>
-             <div className="navbar bg-neutral text-neutral-content">
+  const { user, logOut } = useContext(AuthContext);
+  const handleLogOut = () => {
+    logOut()
+      .then()
+      .catch((error) => {
+        console.log(error.message);
+      });
+  };
+  return (
+    <div>
+      <div className="navbar bg-neutral text-neutral-content">
         <div className="navbar-start">
           <div className="dropdown">
             <label tabIndex={0} className="btn btn-ghost lg:hidden">
@@ -37,14 +37,14 @@ const Navbar = () => {
               tabIndex={0}
               className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
             >
-              <li  className=" font-bold text-orange-400">
-                <Link to='/'>Home</Link>
+              <li className=" font-bold text-orange-400">
+                <Link to="/">Home</Link>
               </li>
-              <li  className=" font-bold text-orange-400">
+              <li className=" font-bold text-orange-400">
                 <a>ABOUT</a>
               </li>
 
-              <li  className=" font-bold text-orange-400">
+              <li className=" font-bold text-orange-400">
                 <a>BLOG</a>
               </li>
             </ul>
@@ -55,30 +55,42 @@ const Navbar = () => {
         </div>
         <div className="navbar-center hidden lg:flex">
           <ul className="menu menu-horizontal px-1">
-            <li  className=" font-bold">
-            <Link to='/home'>Home</Link>
+            <li className=" font-bold">
+              <Link to="/home">Home</Link>
             </li>
-            <li  className=" font-bold">
+            <li className=" font-bold">
               <a>About</a>
             </li>
-            <li  className=" font-bold">
+            <li className=" font-bold">
               <a>Blog</a>
             </li>
           </ul>
         </div>
         <div className="navbar-end">
-         {user && <p className="me-5 text-4xl">
-            <BiUserCircle></BiUserCircle>
-          </p>}
-         
+          {user && 
+            <div className="me-5 text-4xl">
+              <div className="avatar">
+                <div className="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+                  <img src="https://i.ibb.co/p1pyJP1/chef-5.jpg" />
+                </div>
+              </div>
+            </div>
+          }
         </div>
-       <div>
-     {user ?  <button onClick={handleLogOut} className='btn'>Log Out</button> :
-          <Link to='/login' className='btn'><button  className='btn'>Login</button></Link>}
-       </div>
+        <div>
+          {user ? (
+            <button onClick={handleLogOut} className="btn">
+              Log Out
+            </button>
+          ) : (
+            <Link to="/login" className="btn">
+              <button className="btn">Login</button>
+            </Link>
+          )}
+        </div>
       </div>
-        </div>
-    );
+    </div>
+  );
 };
 
 export default Navbar;
--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,17 +1,67 @@
 import React from "react";
 
-import { useLoaderData } from "react-router-dom";
-
+import food1 from "../../../images/food-1.jpg";
+import food2 from "../../../images/food-2.jpg";
+import food3 from "../../../images/food-3.jpg";
 
 const Home = () => {
- 
-  
   return (
-    
-    <div>
-      
+    <div className=" grid lg:grid-cols-3 sm:grid-cols-1 gap-8 mt-20 w-[85%]   mx-auto">
+      <div className="card w-96 bg-base-100 shadow-xl">
+        <div className="avatar">
+          <div className="w-auto rounded">
+            <img src={food1} />
+          </div>
+        </div>
+        <div className="card-body">
+          <h2 className="card-title">
+          NEW!
+            <div className="badge badge-secondary">SPECIAL</div>
+          </h2>
+          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <div className="card-actions justify-end">
+            <div className="badge badge-outline">Fashion</div>
+            <div className="badge badge-outline">Products</div>
+          </div>
+        </div>
+      </div>
+      <div className="card w-96 bg-base-100 shadow-xl">
+        <div className="avatar">
+          <div className="w-auto rounded">
+            <img src={food2} />
+          </div>
+        </div>
+        <div className="card-body">
+          <h2 className="card-title">
+          NEW!
+            <div className="badge badge-secondary">SPECIAL</div>
+          </h2>
+          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <div className="card-actions justify-end">
+            <div className="badge badge-outline">Fashion</div>
+            <div className="badge badge-outline">Products</div>
+          </div>
+        </div>
+      </div>
+      <div className="card w-96 bg-base-100 shadow-xl">
+        <div className="avatar">
+          <div className="w-auto rounded">
+            <img src={food3} />
+          </div>
+        </div>
+        <div className="card-body">
+          <h2 className="card-title">
+            NEW!
+            <div className="badge badge-secondary">SPECIAL</div>
+          </h2>
+          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <div className="card-actions justify-end">
+            <div className="badge badge-outline">Fashion</div>
+            <div className="badge badge-outline">Products</div>
+          </div>
+        </div>
+      </div>
     </div>
-    
   );
 };
 
